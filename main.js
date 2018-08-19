7c5c0daa82e4b1eaac02821507cbedeb5720ac2a
BloxDaCoder <42507803+BloxDaCoder@users.noreply.github.com>
2018-08-19 16:19:03 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,21 +6,25 @@ sql.open("./uptimedatabase.sqlite");
 
 client.on("ready", async () => {
     console.log("Ready")
-    sql.run("CREATE TABLE IF NOT EXISTS livestats (latestuptime INTEGER, lastuptime INTEGER)").then(async () => {
+    sql.run("CREATE TABLE IF NOT EXISTS livestats (latestuptime INTEGER, lastuptime INTEGER, longestuptime INTEGER)").then(async () => {
     sql.get(`SELECT * FROM livestats`).then(async livestatsrow => {
         if (!livestatsrow) {
             if (client.guilds.get(guildID).channels.find(channels => channels.name === "live-stats")) {
                const firstmsg = await client.guilds.get(guildID).channels.find(channels => channels.name === "live-stats").send({embed : {
                     color: 0x7CFC00,
-                  title: `${client.user.username}'s Live Stats Information`,
+                  title: `${client.user.username}'s Uptime Live Stats Information`,
                   description: "Made possible & developed by <@134196203490377729>",
                   fields: [{
                     name: "Last Uptime",
-                    value: "NIL"
+                    value: "N/A"
                   },
                   { 
                     name: `Latest Uptime - Updated every ${interval} interval minute`,
                     value: "0 Minute"
+                  },
+                  { 
+                    name: `Longest Uptime`,
+                    value: "N/A"
                   },
                 ],
                   timestamp: new Date(),
@@ -30,22 +34,26 @@ client.on("ready", async () => {
                   }
                   }
                   });
-                  sql.run("INSERT INTO livestats (latestuptime, lastuptime) VALUES (?, ?)", [0, 0]);
+                  sql.run("INSERT INTO livestats (latestuptime, lastuptime, longestuptime) VALUES (?, ?, ?)", [0, 0, 0]);
                   setInterval(function(){
                     sql.get(`SELECT * FROM livestats`).then(async livestatsrow => {
                         if (livestatsrow) {
                             await sql.run(`UPDATE livestats SET latestuptime = ${livestatsrow.latesuptime + parseInt((interval * 60000))}`);
                             firstmsg.edit({embed : {
                                 color: 0x7CFC00,
-                              title: `${client.user.username}'s Live Stats Information`,
+                              title: `${client.user.username}'s Uptime Live Stats Information`,
                               description: "Made possible & developed by <@134196203490377729>",
                               fields: [{
                                 name: "Last Uptime",
-                                value: `NIL`
+                                value: `N/A`
                               },
                               { 
                                 name: `Latest Uptime - Updated every ${interval} interval minute`,
                                 value: `${(livestatsrow.latestuptime + (interval * 60000)) / 60000} Minute`
+                              },
+                              { 
+                                name: `Longest Uptime`,
+                                value: "N/A"
                               },
                             ],
                               timestamp: new Date(),
@@ -60,17 +68,28 @@ client.on("ready", async () => {
                   }, (interval * 60000))
             }
             else await client.guilds.get(guildID).createChannel("live-stats", "text")
+            await client.guilds.get(guildID).channels.find(channels => channels.name === "live-stats").overwritePermissions(guildID, {
+                READ_MESSAGES: true,
+                SEND_MESSAGES: false
+            })
+            await client.guilds.get(guildID).channels.find(channels => channels.name === "live-stats").overwritePermissions(client.user.id, {
+                SEND_MESSAGES: true
+            })
             const firstmsg = await client.guilds.get(guildID).channels.find(channels => channels.name === "live-stats").send({embed : {
                 color: 0x7CFC00,
-              title: `${client.user.username}'s Live Stats Information`,
+              title: `${client.user.username}'s Uptime Live Stats Information`,
               description: "Made possible & developed by <@134196203490377729>",
               fields: [{
                 name: "Last Uptime",
-                value: "NIL"
+                value: "N/A"
               },
               { 
                 name: `Latest Uptime - Updated every ${interval} interval minute`,
                 value: "0 Minute"
+              },
+              { 
+                name: `Longest Uptime`,
+                value: "N/A"
               },
             ],
               timestamp: new Date(),
@@ -80,22 +99,26 @@ client.on("ready", async () => {
               }
               }
               });
-              sql.run("INSERT INTO livestats (latestuptime, lastuptime) VALUES (?, ?)", [0, 0]);
+              sql.run("INSERT INTO livestats (latestuptime, lastuptime, longestuptime) VALUES (?, ?, ?)", [0, 0, 0]);
               setInterval(function(){
                 sql.get(`SELECT * FROM livestats`).then(async livestatsrow => {
                     if (livestatsrow) {
                         await sql.run(`UPDATE livestats SET latestuptime = ${livestatsrow.latestuptime + parseInt((interval * 60000))}`);
                         firstmsg.edit({embed : {
                             color: 0x7CFC00,
-                          title: `${client.user.username}'s Live Stats Information`,
+                          title: `${client.user.username}'s Uptime Live Stats Information`,
                           description: "Made possible & developed by <@134196203490377729>",
                           fields: [{
                             name: "Last Uptime",
-                            value: "NIL"
+                            value: "N/A"
                           },
                           { 
                             name: `Latest Uptime - Updated every ${interval} interval minute`,
                             value: `${(livestatsrow.latestuptime + (interval * 60000)) / 60000} Minute`
+                          },
+                          { 
+                            name: `Longest Uptime`,
+                            value: "N/A"
                           },
                         ],
                           timestamp: new Date(),
@@ -110,11 +133,74 @@ client.on("ready", async () => {
               }, (interval * 60000))
         }
         else if (livestatsrow) {
-            console.log(client.user.username)
             client.guilds.get(guildID).channels.find(channels => channels.name === "live-stats").fetchMessages({limit : 1}).then(async (fetchedMessages) => {
+                if (livestatsrow.latestuptime >= livestatsrow.longestuptime) {
+                    await fetchedMessages.first().edit({embed : {
+                        color: 0x7CFC00,
+                      title: `${client.user.username}'s Uptime Live Stats Information`,
+                      description: "Made possible & developed by <@134196203490377729>",
+                      fields: [{
+                        name: "Last Uptime",
+                        value: `${livestatsrow.latestuptime / 60000} Minute`
+                      },
+                      { 
+                        name: `Latest Uptime - Updated every ${interval} interval minute`,
+                        value: `0 Minute`
+                      },
+                      { 
+                        name: `Longest Uptime`,
+                        value: `${livestatsrow.latestuptime / 60000} Minute`
+                      },
+                    ],
+                      timestamp: new Date(),
+                      footer: { 
+                      icon_url: client.user.avatarURL,
+                      text: "Live Stats Uptime Counter"
+                      }
+                      }
+                      });
+                  await sql.run(`UPDATE livestats SET lastuptime = ${livestatsrow.latestuptime}`);
+                  await sql.run(`UPDATE livestats SET longestuptime = ${livestatsrow.latestuptime}`);
+                  await sql.run(`UPDATE livestats SET latestuptime = 0`);
+                  setInterval(function(){
+                    sql.get(`SELECT * FROM livestats`).then(async livestatsrow => {
+                        if (livestatsrow) {
+                            await sql.run(`UPDATE livestats SET latestuptime = ${livestatsrow.latestuptime + parseInt((interval * 60000))}`);
+                            fetchedMessages.first().edit({embed : {
+                                color: 0x7CFC00,
+                              title: `${client.user.username}'s Uptime Live Stats Information`,
+                              description: "Made possible & developed by <@134196203490377729>",
+                              fields: [{
+                                name: "Last Uptime",
+                                value: `${livestatsrow.lastuptime / 60000} Minute`
+                              },
+                              { 
+                                name: `Latest Uptime - Updated every ${interval} interval minute`,
+                                value: `${(livestatsrow.latestuptime + (interval * 60000)) / 60000} Minute`
+                              },
+                              { 
+                                name: `Longest Uptime`,
+                                value: `${livestatsrow.longestuptime / 60000} Minute`
+                              },
+                            ],
+                              timestamp: new Date(),
+                              footer: {
+                              icon_url: client.user.avatarURL,
+                              text: "Live Stats Uptime Counter"
+                              }
+                              }
+                              });
+                        }
+                    })
+                  }, (interval * 60000))
+                }
+                else
+                sql.get(`SELECT * FROM livestats`).then(async livestatsrow => {
+                    if (livestatsrow) {
+                    if (livestatsrow.latestuptime < livestatsrow.longestuptime) {
                 await fetchedMessages.first().edit({embed : {
                     color: 0x7CFC00,
-                  title: `${client.user.username}'s Live Stats Information`,
+                  title: `${client.user.username}'s Uptime Live Stats Information`,
                   description: "Made possible & developed by <@134196203490377729>",
                   fields: [{
                     name: "Last Uptime",
@@ -123,6 +209,10 @@ client.on("ready", async () => {
                   { 
                     name: `Latest Uptime - Updated every ${interval} interval minute`,
                     value: `0 Minute`
+                  },
+                  { 
+                    name: `Longest Uptime`,
+                    value: `${livestatsrow.longestuptime / 60000} Minute`
                   },
                 ],
                   timestamp: new Date(),
@@ -140,7 +230,7 @@ client.on("ready", async () => {
                             await sql.run(`UPDATE livestats SET latestuptime = ${livestatsrow.latestuptime + parseInt((interval * 60000))}`);
                             fetchedMessages.first().edit({embed : {
                                 color: 0x7CFC00,
-                              title: `${client.user.username}'s Live Stats Information`,
+                              title: `${client.user.username}'s Uptime Live Stats Information`,
                               description: "Made possible & developed by <@134196203490377729>",
                               fields: [{
                                 name: "Last Uptime",
@@ -149,6 +239,10 @@ client.on("ready", async () => {
                               { 
                                 name: `Latest Uptime - Updated every ${interval} interval minute`,
                                 value: `${(livestatsrow.latestuptime + (interval * 60000)) / 60000} Minute`
+                              },
+                              { 
+                                name: `Longest Uptime`,
+                                value: `${livestatsrow.longestuptime / 60000} Minute`
                               },
                             ],
                               timestamp: new Date(),
@@ -161,6 +255,9 @@ client.on("ready", async () => {
                         }
                     })
                   }, (interval * 60000))
+                }
+            }
+                })
             })
         }
     })
